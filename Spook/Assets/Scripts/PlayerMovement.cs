d344a63e4bc3b9fa678bcf7619f3dc49b77c2f76
Lebrra <Lebrra@users.noreply.github.com>
2019-10-26 12:40:17 -0500

    basic movement; I wanna come back to this one day :)

--- Spook/Assets/Scripts/PlayerMovement.cs
+++ Spook/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float hSpeed = Input.GetAxisRaw("Horizontal");
+        float vSpeed = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector3(hSpeed * speed, vSpeed * speed);
+        transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.FloorToInt(transform.position.y * 2));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other)
+        {
+            Debug.Log("hit");
+        }
     }
 }
